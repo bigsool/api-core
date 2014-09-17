c0ddc0316e849681bc2ef514a1d73e0f83d29560
Julien <julien@bigsool.com>
2014-09-17 18:48:57 +0200

    create ActionContext and modify Context and Registry

---
+++ src/Archiweb/ActionContext.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Archiweb;
+
+
+use Archiweb\Filter\Filter;
+use Archiweb\Rule\Rule;
+
+class ActionContext extends Context {
+
+    /**
+     * @param Context $context
+     */
+    public function __construct (Context $context) {
+        // TODO: Implement constructor
+    }
+
+    /**.
+     * @return Filter[]
+     */
+    public function getFilters () {
+        // TODO: Implement getFilters() method
+    }
+
+    /**
+     * @return Rule[]
+     */
+    public function getRules () {
+        // TODO: Implement getRules() method
+    }
+
+    /**
+     * @param Rule $rule
+     */
+    public function addRule (Rule $rule) {
+        // TODO: Implement addRule() method
+    }
+
+}
--- src/Archiweb/Registry.php
+++ src/Archiweb/Registry.php
@@ -6,4 +6,47 @@ namespace Archiweb;
 
 class Registry {
 
+    /**
+     * @param ActionContext $context
+     */
+    public function __construct (ActionContext $context) {
+        // TODO: Implement constructor
+    }
+
+    /**
+     * @param string $entity
+     * @param array  $params
+     *
+     * @return Object
+     */
+    public function create ($entity, array $params) {
+        // TODO: Implement create() method
+    }
+
+    /**
+     * @param string $entity
+     *
+     * @return Object
+     */
+    public function find ($entity) {
+        // TODO: Implement find() method
+    }
+
+    /**
+     * @param string $joinName
+     *
+     * @return string
+     */
+    public function addJoin ($joinName) {
+        // TODO: Implement addJoin() method
+    }
+
+    /**
+     * @param string $parameter
+     * @param mixed  $value
+     */
+    public function setParameter ($parameter, $value) {
+        // TODO: Implement setParameter() method
+    }
+
 } 
