3392930c6f6bfb63e381011d05732893413853a6
Julien <julien@bigsool.com>
2014-09-18 19:32:31 +0200

    add first part of the test of Registry

--- test/Archiweb/RegistryTest.php
+++ test/Archiweb/RegistryTest.php
@@ -4,6 +4,41 @@
 namespace Archiweb;
 
 
+use Archiweb\Model\Company;
+
 class RegistryTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @var ActionContext
+     */
+    protected $ctx;
+
+    public function setUp () {
+
+        $em = $this->getMockBuilder('\Doctrine\ORM\EntityManager')
+                   ->disableOriginalConstructor()
+                   ->getMock();
+        $ctx = new Context();
+        $ctx->setEntityManager($em);
+        $this->ctx = new ActionContext($ctx);
+
+        $this->registry = new Registry($this->ctx);
+
+    }
+
+    public function testCreate () {
+
+        $company = $this->registry->create('Company', array());
+
+        $this->assertInstanceOf('\Archiweb\Model\Company', $company);
+
+        $this->assertEquals(new Company(), $company);
+
+    }
 
 }
