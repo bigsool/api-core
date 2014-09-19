ad486ee68229c53946e209f8bb2619cbfeaf2136
Julien <julien@bigsool.com>
2014-09-19 17:37:51 +0200

    improve Registry tests and implementation

--- src/Archiweb/Registry.php
+++ src/Archiweb/Registry.php
@@ -4,32 +4,97 @@
 namespace Archiweb;
 
 
+use Archiweb\Parameter\Parameter;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
+
 class Registry {
+
+    /**
+     * @var ActionContext
+     */
+    protected $context;
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
 
     /**
      * @param ActionContext $context
      */
     public function __construct (ActionContext $context) {
-        // TODO: Implement constructor
+
+        $this->context = $context;
+        $this->em = $context->getEntityManager();
+
     }
 
     /**
-     * @param string $entity
-     * @param array  $params
+     * @param string      $entity
+     * @param Parameter[] $params
      *
      * @return Object
      */
     public function create ($entity, array $params) {
-        // TODO: Implement create() method
+
+        $class = self::realModelClassName($entity);
+
+        $obj = new $class;
+        $metadata = $this->em->getClassMetadata($class);
+        $fieldNames = $metadata->getFieldNames();
+
+        foreach ($params as $key => $param) {
+            if (!in_array($key, $fieldNames)) {
+                throw new \RuntimeException("field {$key} not found in the model {$entity}");
+            }
+            if (!($param instanceof Parameter)) {
+                throw new \RuntimeException('invalid type');
+            }
+            if (!$param->isSafe()) {
+                throw new \RuntimeException("unsafe parameter currently not handled");
+            }
+            $methodName = 'set' . ucfirst($key);
+            if (!method_exists($obj, $methodName)) {
+                throw new \RuntimeException("method {$methodName} not found on the model {$entity}");
+            }
+            $obj->$methodName($param->getValue());
+        }
+
+        return $obj;
+
     }
 
     /**
      * @param string $entity
      *
-     * @return Object
+     * @return string
+     */
+    private static function realModelClassName ($entity) {
+
+        $class = '\Archiweb\Model\\' . $entity;
+        if (!class_exists($class)) {
+            throw new \RuntimeException('entity not found');
+        }
+
+        return $class;
+
+    }
+
+    /**
+     * @param string $entity
+     *
+     * @return QueryBuilder
      */
     public function find ($entity) {
-        // TODO: Implement find() method
+
+        $class = self::realModelClassName($entity);
+
+        $qb = $this->em->createQueryBuilder();
+        $qb->from($class, lcfirst($entity));
+
+        return $qb;
+
     }
 
     /**
--- test/Archiweb/RegistryTest.php
+++ test/Archiweb/RegistryTest.php
@@ -5,6 +5,9 @@ namespace Archiweb;
 
 
 use Archiweb\Model\Company;
+use Archiweb\Parameter\Parameter;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 
 class RegistryTest extends \PHPUnit_Framework_TestCase {
 
@@ -20,9 +23,25 @@ class RegistryTest extends \PHPUnit_Framework_TestCase {
 
     public function setUp () {
 
-        $em = $this->getMockBuilder('\Doctrine\ORM\EntityManager')
-                   ->disableOriginalConstructor()
-                   ->getMock();
+        $config =
+            Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/../../doctrine/model/yml"), true,
+                                                   __DIR__ . '/../../src/');
+        $tmpDir = sys_get_temp_dir();
+        $originalDb = $tmpDir . '/archiweb-proto.db.sqlite';
+        $tmpDB = tempnam($tmpDir, 'archiweb-proto.db.sqlite');
+        if (file_exists($originalDb)) {
+            copy($originalDb, $tmpDB);
+        }
+
+        $conn = array(
+            'driver' => 'pdo_sqlite',
+            'path'   => $tmpDB,
+        );
+        $em = EntityManager::create($conn, $config);
+
+        // this query activate the foreign key in sqlite
+        $em->getConnection()->query('PRAGMA foreign_keys = ON');
+
         $ctx = new Context();
         $ctx->setEntityManager($em);
         $this->ctx = new ActionContext($ctx);
@@ -34,10 +53,65 @@ class RegistryTest extends \PHPUnit_Framework_TestCase {
     public function testCreate () {
 
         $company = $this->registry->create('Company', array());
-
         $this->assertInstanceOf('\Archiweb\Model\Company', $company);
-
         $this->assertEquals(new Company(), $company);
+
+        $company = $this->registry->create('Company', array('name' => $this->getParameterMock('company name', true)));
+        $this->assertInstanceOf('\Archiweb\Model\Company', $company);
+        $this->assertEquals('company name', $company->getName());
+
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return Parameter
+     */
+    protected function getParameterMock ($value, $isSafe) {
+
+        $parameter = $this->getMockBuilder('\Archiweb\Parameter\Parameter')
+                          ->disableOriginalConstructor()
+                          ->getMock();
+        $parameter->method('isSafe')->willReturn($isSafe);
+        $parameter->method('getValue')->willReturn($value);
+
+        return $parameter;
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testCreateWithFieldNotExists () {
+
+        $this->registry->create('Company', array('qwe' => $this->getParameterMock('qwe', true)));
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testEntityNotFound () {
+
+        $this->registry->create('Qwe', array());
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testInvalidParameterType () {
+
+        $this->registry->create('Company', array('name' => 'qwe'));
+
+    }
+
+    public function testFind () {
+
+        $qb = $this->registry->find('Company');
+        $this->assertInstanceOf('\Doctrine\ORM\QueryBuilder', $qb);
+        $dql = $qb->getDQL();
+        $this->assertEquals('SELECT company FROM \Archiweb\Model\Company company', $dql);
 
     }
 
