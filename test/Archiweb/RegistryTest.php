5ff4d474302d97ab4777ca897449ee44d23833db
Julien <julien@bigsool.com>
2014-09-29 17:38:18 +0200

    testing Registry::find()

--- test/Archiweb/RegistryTest.php
+++ test/Archiweb/RegistryTest.php
@@ -32,12 +32,18 @@ class RegistryTest extends TestCase {
 
         $product = new Product();
         $product->setName('produit 1');
+        $product->setBundleid('produit-1');
+        $product->setConsumable(false);
+        $product->setPrice(17);
+        $product->setWeight(0);
+        $product->setAvailable(true);
+        $product->setVat(.2);
 
         $registry = self::$appCtx->getNewRegistry();
         $registry->save($product);
 
     }
-
+/*
     public function testSaveWithDependencies () {
 
         $company = new Company();
@@ -55,7 +61,7 @@ class RegistryTest extends TestCase {
         $registry->save($company);
 
     }
-
+*/
     /**
      * @expectedException \Exception
      */
@@ -69,7 +75,7 @@ class RegistryTest extends TestCase {
     /**
      * @depends testSave
      */
-    public function testFindWithoutFilter () {
+    public function testFindWithoutFilterAsArray () {
 
         $qryCtx = $this->getFindQueryContext('Produit');
         $qryCtx->addField(self::$appCtx->getFieldByEntityAndName('Produit', '*'));
@@ -78,6 +84,26 @@ class RegistryTest extends TestCase {
         $result = $registry->find($qryCtx);
 
         $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+        $this->assertSame([], $result[0]);
+        // TODO: improve test
+
+    }
+
+    /**
+     * @depends testSave
+     */
+    public function testFindWithoutFilterAsObject () {
+
+        $qryCtx = $this->getFindQueryContext('Produit');
+        $qryCtx->addField(self::$appCtx->getFieldByEntityAndName('Produit', '*'));
+
+        $registry = self::$appCtx->getNewRegistry();
+        $result = $registry->find($qryCtx, false);
+
+        $this->assertInternalType('array', $result);
+        $this->assertCount(1, $result);
+        $this->assertSame([], $result[0]);
         // TODO: improve test
 
     }
