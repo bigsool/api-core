3ec15fe63e98004b8da3f36be418dfda00876963
Julien <julien@bigsool.com>
2014-10-03 17:53:46 +0200

    improve the UseCasesTest class

--- test/Archiweb/UseCasesTest.php
+++ test/Archiweb/UseCasesTest.php
@@ -5,30 +5,66 @@ namespace Archiweb;
 
 use Archiweb\Context\ActionContext;
 use Archiweb\Context\ApplicationContext;
+use Archiweb\Context\FindQueryContext;
+use Archiweb\Context\QueryContext;
 use Archiweb\Context\RequestContext;
+use Archiweb\Expression\BinaryExpression;
+use Archiweb\Expression\KeyPath;
+use Archiweb\Expression\Parameter;
+use Archiweb\Field\StarField;
+use Archiweb\Filter\ExpressionFilter;
+use Archiweb\Filter\StringFilter;
 use Archiweb\Model\Company;
+use Archiweb\Model\HostedProject;
+use Archiweb\Model\SharedHostedProject;
 use Archiweb\Model\Storage;
 use Archiweb\Model\User;
+use Archiweb\Operator\MemberOf;
 use Archiweb\Parameter\SafeParameter;
 use Archiweb\Parameter\UnsafeParameter;
+use Archiweb\Rule\SimpleRule;
 
 class UseCasesTest extends TestCase {
 
     /**
      * @var ApplicationContext
      */
-    protected $appCtx;
+    protected static $appCtx;
 
-    public function setUp () {
+    public static function  setUpBeforeClass () {
 
-        $this->appCtx = $this->getApplicationContext();
-        $this->resetDatabase($this->appCtx);
+        parent::setUpBeforeClass();
+
+        self::$appCtx = self::getApplicationContext();
+        self::resetDatabase(self::$appCtx);
+
+        self::$appCtx->addField(new StarField('User'));
+        self::$appCtx->addField(new StarField('HostedProject'));
+
+        self::$appCtx->addFilter(new StringFilter('HostedProject', 'withSharedProject',
+                                                  'self.sharedHostedProjects.participant = :authUser', 'SELECT'));
+
+        $binary =
+            new BinaryExpression(new MemberOf(), new Parameter(':authUser'), new KeyPath('creator.company.users'));
+        self::$appCtx->addFilter(new ExpressionFilter('HostedProject', 'fromMyCompany', 'SELECT', $binary));
+        self::$appCtx->addRule(new SimpleRule('fromMyCompanyRule', function (QueryContext $context) {
+
+            if ($context instanceof FindQueryContext) {
+
+                // TODO: il faut aussi gérer dans le cas ou HostedProject fasse partie des joins
+                return $context->getEntity() == 'HostedProject';
+
+            }
+
+            return false;
+
+        }, self::$appCtx->getFilterByEntityAndName('HostedProject', 'fromMyCompany')));
 
     }
 
     public function testCreateUser () {
 
-        $reqCtx = new RequestContext($this->appCtx);
+        $reqCtx = new RequestContext(self::$appCtx);
         $reqCtx->setParams(['user'    => ['email'     => 'qwe@qwe.come',
                                           'password'  => 'qwe',
                                           'name'      => 'Ferrier',
@@ -180,6 +216,180 @@ class UseCasesTest extends TestCase {
         $registry->save($user);
 
         $actCtx['user'] = $user;
+
+    }
+
+    public function testListProjects () {
+
+        $this->createSomeUsersAndProjects();
+
+        $reqCtx = new RequestContext(self::$appCtx);
+        $reqCtx->setParams([]);
+
+        $actCtx = $reqCtx->getNewActionContext();
+        $result = $this->getUser('User 1');
+        $this->assertCount(1, $result);
+        $actCtx['authUser'] = $result[0];
+        $this->assertInstanceOf('\Archiweb\Model\User', $actCtx['authUser']);
+        $this->assertSame('User 1', $actCtx['authUser']->getName());
+
+        $qryCtx = new FindQueryContext(self::$appCtx, 'HostedProject');
+        $qryCtx->addKeyPath(new KeyPath('*'));
+        $qryCtx->setParams(['authUser'=>$actCtx['authUser']]);
+
+        $registry = self::$appCtx->getNewRegistry();
+        $result = $registry->find($qryCtx);
+
+
+
+    }
+
+    protected function createSomeUsersAndProjects () {
+
+        $user1 = $this->createUser(['name' => 'User 1']);
+        $company1 = $this->createCompany(['name' => 'Company 1']);
+        $this->setUserToCompany($user1, $company1);
+
+        $user2 = $this->createUser(['name' => 'User 2']);
+        $company1 = $this->createCompany(['name' => 'Company 2']);
+        $this->setUserToCompany($user2, $company1);
+
+        $this->createProject('Projet de 1', $user1);
+        $this->createProject('Projet de 1 partagé avec 2', $user1, [$user2]);
+        $this->createProject('Projet de 2', $user2);
+        $this->createProject('Projet de 2 partagé avec 1', $user2, [$user1]);
+
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return User
+     */
+    protected function createUser (array $params = []) {
+
+        $user = new User();
+        $user->setEmail($this->d($params, 'email', uniqid()));
+        $user->setPassword($this->d($params, 'password'));
+        $user->setName($this->d($params, 'name'));
+        $user->setFirstname($this->d($params, 'firstname'));
+        $user->setLang($this->d($params, 'lang'));
+        $user->setKnowsfrom($this->d($params, 'knowsFrom'));
+        $user->setRegisterDate(new \DateTime());
+        $user->setLastLoginDate(new \DateTime());
+        $user->setConfirmationkey(uniqid());
+        $user->setSalt(uniqid());
+
+        self::$appCtx->getNewRegistry()->save($user);
+
+        return $user;
+
+    }
+
+    /**
+     * @param array  $arr
+     * @param        $key
+     * @param string $default
+     *
+     * @return string
+     */
+    protected function d (array &$arr, $key, $default = '') {
+
+        return isset($arr[$key]) ? $arr[$key] : $default;
+
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return User
+     */
+    protected function createCompany (array $params = []) {
+
+        $company = new Company();
+        $company->setName($this->d($params, 'name'));
+        $company->setAddress($this->d($params, 'address'));
+        $company->setZipCode($this->d($params, 'zipCode'));
+        $company->setCity($this->d($params, 'city'));
+        $company->setState($this->d($params, 'state'));
+        $company->setCountry($this->d($params, 'country'));
+        $company->setTel($this->d($params, 'tel'));
+        $company->setFax($this->d($params, 'fax'));
+        $company->setTva($this->d($params, 'tva'));
+
+        self::$appCtx->getNewRegistry()->save($company);
+
+        return $company;
+
+    }
+
+    /**
+     * @param User    $user
+     * @param Company $company
+     */
+    protected function setUserToCompany (User &$user, Company &$company) {
+
+        $user->setCompany($company);
+        $user->setOwnedCompany($company);
+        $company->setOwner($user);
+        $company->addUser($user);
+
+        self::$appCtx->getNewRegistry()->save($user);
+
+    }
+
+    /**
+     * @param        $name
+     * @param User   $owner
+     * @param User[] $participants
+     *
+     * @return HostedProject
+     */
+    protected function createProject ($name, User $owner, array $participants = []) {
+
+        $project = new HostedProject();
+        $project->setName($name);
+        $project->setClientNameCreator('');
+        $project->setClientVersionCreator('');
+        $project->setCreationDate(new \DateTime());
+        $project->setCreator($owner);
+        $owner->addHostedProject($project);
+        $project->setIsSynchronizable(true);
+        $project->setIsUploading(false);
+        $project->setLastModificationDate(new \DateTime());
+        $project->setPatchId('');
+        $project->setUUIDCreator('');
+
+        self::$appCtx->getNewRegistry()->save($project);
+
+        foreach ($participants as $participant) {
+            $shp = new SharedHostedProject();
+            $shp->setPermission('');
+            $shp->setHostedProject($project);
+            $shp->setParticipant($participant);
+            $participant->addSharedHostedProject($shp);
+            $project->addSharedHostedProject($shp);
+        }
+
+        self::$appCtx->getNewRegistry()->save($project);
+
+        return $project;
+
+    }
+
+    /**
+     * @param $name
+     *
+     * @return array
+     */
+    protected function getUser ($name) {
+
+        $findCtx = new FindQueryContext(self::$appCtx, 'User');
+        $findCtx->addFilter(new StringFilter('User', '', 'name = "' . $name . '"', 'SELECT'));
+        $findCtx->addKeyPath(new KeyPath('*'));
+        $user = self::$appCtx->getNewRegistry()->find($findCtx, false);
+
+        return $user;
 
     }
 
