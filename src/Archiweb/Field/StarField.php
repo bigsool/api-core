68474c62b538e16cf76bc987662325b92e7a1901
Julien <julien@bigsool.com>
2014-10-06 10:35:40 +0200

    fix StarField constructor

--- src/Archiweb/Field/StarField.php
+++ src/Archiweb/Field/StarField.php
@@ -3,18 +3,15 @@
 
 namespace Archiweb\Field;
 
-use Archiweb\Rule\Rule;
-
 
 class StarField extends Field {
 
     /**
      * @param string $entity
-     * @param Rule   $rule
      */
-    public function __construct ($entity, Rule $rule = NULL) {
+    public function __construct ($entity) {
 
-        parent::__construct($entity, '*', $rule);
+        parent::__construct($entity, '*');
 
     }
 
