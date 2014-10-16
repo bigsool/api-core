ca6448e19fdb99ec2776c89ad3f949802afae7f8
Julien <julien@bigsool.com>
2014-10-16 15:56:31 +0200

    creation of a new constraint to specify that a value must be an array

---
+++ src/Archiweb/Validation/Constraints/Tab.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Archiweb\Validation\Constraints;
+
+
+use Symfony\Component\Validator\Constraint;
+
+
+class Tab extends Constraint {
+
+    public $message = 'must be an array';
+
+} 
