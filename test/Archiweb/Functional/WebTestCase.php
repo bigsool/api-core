00f669657abe32054335f12ad716af464f9bd370
thierry <thierry@yopmail.com>
2014-11-05 15:47:41 +0100

    Fix base URL

--- test/Archiweb/Functional/WebTestCase.php
+++ test/Archiweb/Functional/WebTestCase.php
@@ -73,7 +73,7 @@ abstract class WebTestCase extends \PHPUnit_Framework_TestCase {
     public static function get ($service, $method, array $params = [], $entity = NULL, array $fields = [],
                                 $auth = NULL, $id = NULL) {
 
-        $config = ['base_url' => 'http://localhost/archipad-proto/run.php/JSON/archipad-cloud+1+fr/'];
+        $config = ['base_url' => 'http://localhost/archiweb-proto/run.php/JSON/archipad-cloud+1+fr/'];
         if (version_compare(PHP_VERSION, '5.5.0') >= 0) {
             $config['handler'] = new CurlHandler();
         }
