2a49b6aff42eeb3f6b26491e1d7ff8f18d57ea8b
thierry <thierry@yopmail.com>
2014-12-17 16:43:41 +0100

    Commit to merge

--- src/Core/Module/MagicalModuleManager.php
+++ src/Core/Module/MagicalModuleManager.php
@@ -4,14 +4,29 @@
 namespace Core\Module;
 
 
+use Core\Action\SimpleAction;
+use Core\Context\ApplicationContext;
 use Core\Parameter\Parameter;
+use Symfony\Component\Validator\Validation;
 
 abstract class MagicalModuleManager extends ModuleManager {
+
+    private $modelAspects;
 
     /**
      * @param array $config
      */
     protected function addAspect (array $config) {
+        $this->modelAspects[] = new ModelAspect($config['model'],
+                                                $config['prefix'],
+                                                $config['constraints'],
+                                                $config['keyPath']);
+    }
+
+    /**
+     * @return ModelAspect[]
+     */
+    protected function getAspects() {
 
     }
 
@@ -20,14 +35,30 @@ abstract class MagicalModuleManager extends ModuleManager {
      */
     protected function magicalCreate (array $params) {
 
+        // foreach modelAspects
+             // validate()
+            // entitiesCreated[] = call BasicHelper create with param
+        foreach ($this->modelAspects as $modelAspect) {
+            $param = $params[$modelAspect->getPrefix()];
+            Validation::createValidator()->validate($param, $modelAspect->getConstrains());
+            $basicHelper = new BasicHelper();
+            $models[] = $basicHelper->create($modelAspect->getModel(),$params);
+        }
+
+        return models;
+
     }
 
     /**
      * @param string   $name
      * @param callable $processFn
      */
-    protected function defineAction ($name, callable $processFn) {
-
+    protected function defineAction ($name, $params, callable $processFn) {
+        $className = get_called_class();
+        $classNameExploded = explode('\\',$className);
+        $module = $classNameExploded[count($classNameExploded) - 2];
+        $appCtx = ApplicationContext::getInstance();
+        $appCtx->addAction(new SimpleAction($module,$name,[], $params,$processFn));
     }
 
 } 
--- src/Core/Module/ModelAspect.php
+++ src/Core/Module/ModelAspect.php
@@ -7,26 +7,45 @@ namespace Core\Module;
 use Core\Expression\AbstractKeyPath;
 use Core\Validation\ConstraintsProvider;
 
-interface ModelAspect {
+class ModelAspect {
 
+    private $model;
+    private $prefix;
+    private $constraints;
+    private $keyPath;
+
+    function __construct($model, $prefix, $constraints, $keyPath) {
+        $this->model = $model;
+        $this->prefix = $prefix;
+        $this->constraints = $constraints;
+        $this->keyPath = $keyPath;
+    }
     /**
      * @return string
      */
-    public function getModel ();
+    public function getModel () {
+        return $this->model;
+    }
 
     /**
      * @return string|null
      */
-    public function getPrefix ();
+    public function getPrefix () {
+        return $this->prefix;
+    }
 
     /**
      * @return ConstraintsProvider[]
      */
-    public function getValidators ();
+    public function getConstraints () {
+        return $this->constraints;
+    }
 
     /**
      * @return AbstractKeyPath|null
      */
-    public function getKeyPath ();
+    public function getKeyPath () {
+        return $this->keyPath;
+    }
 
 } 
