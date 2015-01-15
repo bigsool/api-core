3353f9c5915a4202e5980040a464130b1612f1c3
Julien <julien@bigsool.com>
2015-01-15 19:17:45 +0100

    fix MagicalModuleManager

--- src/Core/Module/MagicalModuleManager.php
+++ src/Core/Module/MagicalModuleManager.php
@@ -20,7 +20,7 @@ use Symfony\Component\Validator\Validation;
 abstract class MagicalModuleManager extends ModuleManager {
 
     /**
-     * @var array
+     * @var ModelAspect[]
      */
     private $modelAspects = [];
 
@@ -30,79 +30,6 @@ abstract class MagicalModuleManager extends ModuleManager {
     private $models = [];
 
     /**
-     * @param array $config
-     */
-    protected function addAspect (array $config) {
-
-        $prefix = NULL;
-        if (isset($config['prefix'])) {
-            $prefix = $config['prefix'];
-            if (!is_string($prefix)) {
-                throw new \RuntimeException('invalid model');
-            }
-        }
-
-        $model = NULL;
-        if (isset($config['model'])) {
-            $model = $config['model'];
-            Registry::realModelClassName($model);
-            if (!is_string($model)) {
-                throw new \RuntimeException('invalid model');
-            }
-        }
-
-
-        $constraints = [];
-        if (isset($config['constraints'])) {
-            $constraints = $config['constraints'];
-            if (!is_array($config['constraints'])) {
-                throw new \RuntimeException('invalid constraints');
-            }
-            foreach ($constraints as $constraint) {
-                if (!is_a($constraint, 'Symfony\Component\Validator\Constraint')
-                    && !is_a($constraint, 'Core\Validation\Constraints\Dictionary')
-                ) {
-                    throw new \RuntimeException('invalid constraints');
-                }
-            }
-        }
-
-        $actions = [];
-        if (isset($config['actions'])) {
-            if (!is_array($config['actions'])) {
-                throw new \RuntimeException('invalid constraints');
-            }
-            foreach ($config['actions'] as $action) {
-                if ($action && !is_a($action, 'Core\Action\Action')) {
-                    throw new \RuntimeException('invalid action');
-                }
-            }
-            $actions = $config['actions'];
-        }
-
-        $keyPath = isset($config['keyPath']) ? new KeyPath($config['keyPath']) : NULL;
-
-        if (!$keyPath) {
-            foreach ($this->modelAspects as $modelAspect) {
-                if (!$modelAspect->getKeyPath()) {
-                    throw new \RuntimeException('two main entities');
-                }
-            }
-        }
-
-        $this->modelAspects[] = new ModelAspect($model, $prefix, $constraints, $actions, $keyPath);
-
-    }
-
-    /**
-     * @return ModelAspect[]
-     */
-    protected function getAspects () {
-
-        return $this->modelAspects;
-    }
-
-    /**
      * @param ActionContext $ctx
      *
      * @return mixed
@@ -110,16 +37,6 @@ abstract class MagicalModuleManager extends ModuleManager {
     public function magicalCreate (ActionContext $ctx) {
 
         return $this->magicalModify($ctx, 'create');
-    }
-
-    /**
-     * @param ActionContext $ctx
-     *
-     * @return mixed
-     */
-    public function magicalUpdate (ActionContext $ctx) {
-
-        return $this->magicalModify($ctx, 'update');
     }
 
     /**
@@ -201,6 +118,101 @@ abstract class MagicalModuleManager extends ModuleManager {
     }
 
     /**
+     * @param ModelAspect $modelAspect
+     *
+     * @return string
+     */
+    private function isMainEntity ($modelAspect) {
+
+        return !$modelAspect->getKeyPath();
+    }
+
+    private function setRelationshipsFromMetadata () {
+
+        $modelNameForKeyPath = [];
+
+        foreach ($this->modelAspects as $modelAspect) {
+
+            if ($this->isMainEntity($modelAspect)) {
+                continue;
+            }
+
+            $keyPath = $modelAspect->getKeyPath()->getValue();
+            $modelNameForKeyPath[$keyPath] = $modelAspect->getModel();
+
+            // Si prefix ne contient pas de . alors on se base de mainEntity
+            $pos = strrpos($keyPath, '.');
+            if ($pos === false) {
+                $modelName = $this->getMainEntityName();
+                $lastKeyPath = $keyPath;
+            }
+            else {
+                if (!isset($modelNameForKeyPath[substr($keyPath, 0, $pos)])) {
+                    throw new \RuntimeException('model name not defined for this prefix');
+                }
+                $modelName = $modelNameForKeyPath[substr($keyPath, 0, $pos)];
+                $lastKeyPath = substr($keyPath, $pos + 1);
+            }
+
+            $mainEntityClassName = Registry::realModelClassName($modelName);
+            $metadata = ApplicationContext::getInstance()->getClassMetadata($mainEntityClassName);
+            $mapping = $metadata->getAssociationMapping($lastKeyPath);
+
+            $this->setRelationshipsFromAssociationMapping($modelName, $mapping);
+
+        }
+
+    }
+
+    private function getMainEntityName () {
+
+        foreach ($this->modelAspects as $modelAspect) {
+            if ($modelAspect->getKeyPath() == '') {
+                return $modelAspect->getModel();
+            }
+        }
+
+        return NULL;
+    }
+
+    /**
+     * @param string $sourceModelName
+     * @param        $mapping
+     */
+    private function setRelationshipsFromAssociationMapping ($sourceModelName, array $mapping) {
+
+        $target = explode('\\', $mapping['targetEntity']);
+        $target = $target[count($target) - 1];
+
+        /*if (!array_key_exists($target, $this->models)) {
+            continue;
+        }*/
+
+        $field1 = $mapping['fieldName'];
+        $field2 = $mapping['mappedBy'];
+
+        $prefix1 = 'set';
+        $prefix2 = 'set';
+
+        if ($mapping['type'] == ClassMetadataInfo::ONE_TO_MANY || $mapping['type'] == ClassMetadataInfo::MANY_TO_MANY) {
+            $field1 = substr($field1, 0, strlen($field1) - 1);
+            $prefix1 = "add";
+        }
+
+        if ($mapping['type'] == ClassMetadataInfo::MANY_TO_ONE || $mapping['type'] == ClassMetadataInfo::MANY_TO_MANY) {
+            $field2 = $mapping['inversedBy'];
+            $field2 = substr($field2, 0, strlen($field2) - 1);
+            $prefix2 = "add";
+        }
+
+        $fn = $prefix1 . ucfirst($field1);
+        $this->models[$sourceModelName]->$fn($this->models[$target]);
+
+        $fn = $prefix2 . ucfirst($field2);
+        $this->models[$target]->$fn($this->models[$sourceModelName]);
+    }
+
+    /**
      * @return mixed
      */
     private function loadEntities ($mainEntity) {
@@ -232,50 +244,87 @@ abstract class MagicalModuleManager extends ModuleManager {
 
     }
 
-    private function setRelationshipsFromMetadata () {
+    /**
+     * @param ActionContext $ctx
+     *
+     * @return mixed
+     */
+    public function magicalUpdate (ActionContext $ctx) {
 
-        $appCtx = ApplicationContext::getInstance();
+        return $this->magicalModify($ctx, 'update');
+    }
 
-        $mainEntityName = $this->getMainEntityName();
-        $metadata = $appCtx->getClassMetadata('Core\Model\\' . $mainEntityName);
-        $mapping = $metadata->getAssociationMappings();
+    /**
+     * @param array $config
+     */
+    protected function addAspect (array $config) {
 
-        foreach ($mapping as $elem) {
-
-            $source = $mainEntityName;
-
-            $target = explode('\\', $elem['targetEntity']);
-            $target = $target[count($target) - 1];
-
-            if (!array_key_exists($target, $this->models)) {
-                continue;
+        $prefix = NULL;
+        if (isset($config['prefix'])) {
+            $prefix = $config['prefix'];
+            if (!is_string($prefix)) {
+                throw new \RuntimeException('invalid model');
             }
-
-            $field1 = $elem['fieldName'];
-            $field2 = $elem['mappedBy'];
-
-            $prefix1 = 'set';
-            $prefix2 = 'set';
-
-            if ($elem['type'] == ClassMetadataInfo::ONE_TO_MANY || $elem['type'] == ClassMetadataInfo::MANY_TO_MANY) {
-                $field1 = substr($field1, 0, strlen($field1) - 1);
-                $prefix1 = "add";
-            }
-
-            if ($elem['type'] == ClassMetadataInfo::MANY_TO_ONE || $elem['type'] == ClassMetadataInfo::MANY_TO_MANY) {
-                $field2 = $elem['inversedBy'];
-                $field2 = substr($field2, 0, strlen($field2) - 1);
-                $prefix2 = "add";
-            }
-
-            $fn = $prefix1 . ucfirst($field1);
-            $this->models[$source]->$fn($this->models[$target]);
-
-            $fn = $prefix2 . ucfirst($field2);
-            $this->models[$target]->$fn($this->models[$source]);
-
         }
 
+        $model = NULL;
+        if (isset($config['model'])) {
+            $model = $config['model'];
+            Registry::realModelClassName($model);
+            if (!is_string($model)) {
+                throw new \RuntimeException('invalid model');
+            }
+        }
+
+
+        $constraints = [];
+        if (isset($config['constraints'])) {
+            $constraints = $config['constraints'];
+            if (!is_array($config['constraints'])) {
+                throw new \RuntimeException('invalid constraints');
+            }
+            foreach ($constraints as $constraint) {
+                if (!is_a($constraint, 'Symfony\Component\Validator\Constraint')
+                    && !is_a($constraint, 'Core\Validation\Constraints\Dictionary')
+                ) {
+                    throw new \RuntimeException('invalid constraints');
+                }
+            }
+        }
+
+        $actions = [];
+        if (isset($config['actions'])) {
+            if (!is_array($config['actions'])) {
+                throw new \RuntimeException('invalid constraints');
+            }
+            foreach ($config['actions'] as $action) {
+                if ($action && !is_a($action, 'Core\Action\Action')) {
+                    throw new \RuntimeException('invalid action');
+                }
+            }
+            $actions = $config['actions'];
+        }
+
+        $keyPath = isset($config['keyPath']) ? new KeyPath($config['keyPath']) : NULL;
+
+        if (!$keyPath) {
+            foreach ($this->modelAspects as $modelAspect) {
+                if (!$modelAspect->getKeyPath()) {
+                    throw new \RuntimeException('two main entities');
+                }
+            }
+        }
+
+        $this->modelAspects[] = new ModelAspect($model, $prefix, $constraints, $actions, $keyPath);
+
+    }
+
+    /**
+     * @return ModelAspect[]
+     */
+    protected function getAspects () {
+
+        return $this->modelAspects;
     }
 
     protected function magicalFind ($keyPaths, $filters) {
@@ -302,30 +351,9 @@ abstract class MagicalModuleManager extends ModuleManager {
 
     }
 
-    private function getMainEntityName () {
-
-        foreach ($this->modelAspects as $modelAspect) {
-            if ($modelAspect->getKeyPath() == '') {
-                return $modelAspect->getModel();
-            }
-        }
-
-        return NULL;
-    }
-
     /**
-     * @param string $modelAspect
-     *
-     * @return string
-     */
-    private function isMainEntity ($modelAspect) {
-
-        return !$modelAspect->getKeyPath();
-    }
-
-    /**
-     * @param string $name
-     * @param array $params
+     * @param string   $name
+     * @param array    $params
      * @param callable $processFn
      */
     protected function defineAction ($name, Array $params, callable $processFn) {
