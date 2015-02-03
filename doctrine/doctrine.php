d525c25b953e6fa1b053bdefba631d9742bc6970
Julien <julien@bigsool.com>
2015-02-03 10:43:18 +0100

    add Migration commands

--- doctrine/doctrine.php
+++ doctrine/doctrine.php
@@ -3,6 +3,14 @@
 $helperSet = require __DIR__ . '/cli-config.php';
 
 \Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet, [
-    new \Core\Doctrine\Command\BuildEntitiesCommand
+    new \Core\Doctrine\Command\BuildEntitiesCommand,
+
+    // Migrations Commands
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand(),
 ]);
  
