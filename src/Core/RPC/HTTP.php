8507e8e435e8926ace6380b9d7a58bb78805d69f
Julien <julien@bigsool.com>
2015-02-05 16:18:07 +0100

    add HTTP RPC Handler

---
+++ src/Core/RPC/HTTP.php
@@ -0,0 +1,218 @@
+<?php
+
+
+namespace Core\RPC;
+
+
+use Core\Context\ApplicationContext;
+use Core\Error\FormattedError;
+use Core\Serializer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HTTP implements Handler {
+
+    /**
+     * @var string
+     */
+    protected $clientName;
+
+    /**
+     * @var string
+     */
+    protected $clientVersion;
+
+    /**
+     * @var string
+     */
+    protected $locale;
+
+    /**
+     * @var string
+     */
+    protected $service;
+
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * @var string
+     */
+    protected $ipAddress;
+
+    /**
+     * @var string|null
+     */
+    protected $returnedRootEntity;
+
+    /**
+     * @var string[]
+     */
+    protected $returnedFields;
+
+    /**
+     * @return string
+     */
+    public function getClientName () {
+
+        return $this->clientName;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientVersion () {
+
+        return $this->clientVersion;
+
+    }
+
+    /**
+     * @param FormattedError $error
+     *
+     * @return Response
+     */
+    public function getErrorResponse (FormattedError $error) {
+
+        return new Response(print_r($error, true), Response::HTTP_INTERNAL_SERVER_ERROR);
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress () {
+
+        return $this->ipAddress;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale () {
+
+        return $this->locale;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod () {
+
+        return $this->method;
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams () {
+
+        return $this->params;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath () {
+
+        return $this->path;
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getReturnedFields () {
+
+        return $this->returnedFields;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnedRootEntity () {
+
+        return $this->returnedRootEntity;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getService () {
+
+        return $this->service;
+    }
+
+    /**
+     * @param Serializer $serializer
+     * @param mixed      $data
+     *
+     * @return Response
+     */
+    public function getSuccessResponse (Serializer $serializer, $data) {
+
+        return new Response(print_r($data, true), RESPONSE::HTTP_OK);
+
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws FormattedError
+     */
+    public function parse (Request $request) {
+
+        $explodedPathInfo = explode('/', trim($request->getPathInfo(), '/'));
+
+        if (!isset($explodedPathInfo[1])) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_CLIENT_IS_INVALID);
+        }
+        $explodedClient = explode('+', $explodedPathInfo[1]);
+        if (count($explodedClient) != 3) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_CLIENT_IS_INVALID);
+        }
+        list($this->clientName, $this->clientVersion, $this->locale) = $explodedClient;
+        if ($this->locale != 'fr') {
+            $this->locale = 'en';
+        }
+
+        if (!isset($explodedPathInfo[2])) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_SERVICE_NOT_FOUND);
+        }
+        $this->service = $explodedPathInfo[2];
+
+        if (!isset($explodedPathInfo[3])) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_METHOD_NOT_FOUND);
+        }
+        $this->method = $explodedPathInfo[3];
+
+        $this->path = '/' . $this->service . '/' . $this->method;
+
+        $this->params = array_merge($request->query->all(), $request->request->all());
+
+        $this->ipAddress = $request->getClientIp();
+
+        $this->returnedRootEntity = NULL;
+        $this->returnedFields = [];
+
+    }
+}
