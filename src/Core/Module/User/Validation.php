c970c5eaa94d81062181274805726dc482c65a08
Julien <julien@bigsool.com>
2015-03-05 17:41:59 +0100

    improve User\Validation

--- src/Core/Module/User/Validation.php
+++ src/Core/Module/User/Validation.php
@@ -7,6 +7,7 @@ use Core\Validation\AbstractConstraintsProvider;
 use Core\Validation\Parameter\Choice;
 use Core\Validation\Parameter\Length;
 use Core\Validation\Parameter\NotBlank;
+use Core\Validation\Parameter\NotNull;
 use Core\Validation\Parameter\String;
 
 class Validation extends AbstractConstraintsProvider {
@@ -21,12 +22,14 @@ class Validation extends AbstractConstraintsProvider {
                 [
                     new String(),
                     new Length(['max' => 255]),
+                    new NotNull(),
                 ]
             ,
             'lastName'  =>
                 [
                     new String(),
                     new Length(['max' => 255]),
+                    new NotNull(),
                 ]
             ,
             'lang'      =>
