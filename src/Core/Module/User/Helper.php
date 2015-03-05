2f8fcaa9348ba8038f3f7e1f9eee0ab2c9e5e4c5
Julien <julien@bigsool.com>
2015-03-05 15:01:37 +0100

    fix Core\User\Helper

--- src/Core/Module/User/Helper.php
+++ src/Core/Module/User/Helper.php
@@ -5,8 +5,10 @@ namespace Core\Module\User;
 
 
 use Core\Context\ActionContext;
+use Core\Context\ApplicationContext;
 use Core\Model\User;
 use Core\Module\BasicHelper;
+use Core\Registry;
 
 class Helper extends BasicHelper {
 
@@ -16,8 +18,12 @@ class Helper extends BasicHelper {
      */
     public function createUser (ActionContext $actCtx, array $params) {
 
-        $user = new User();
+        $userClassName = $this->realModelClassName('User');
+        $user = new $userClassName;
 
+        /**
+         * @var User $user
+         */
         $user->setCreationDate(new \DateTime());
 
         $this->basicSave($user, $params);
@@ -26,7 +32,16 @@ class Helper extends BasicHelper {
 
     }
 
-    public function updateUser (ActionContext $actCtx, User $user, array $params) {
+    /**
+     * @param ActionContext $actCtx
+     * @param               $user
+     * @param array         $params
+     */
+    public function updateUser (ActionContext $actCtx, $user, array $params) {
+
+        if (!is_a($user, $this->realModelClassName('User'))) {
+            throw new \RuntimeException('Unexpected type for $user');
+        }
 
         $this->basicSave($user, $params);
 
