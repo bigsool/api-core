54d149f437a24108dcb7d8dd0af305f8aef20c97
Julien <julien@bigsool.com>
2015-03-05 10:23:13 +0100

    add JSONP protocol handler

---
+++ src/Core/RPC/JSONP.php
@@ -0,0 +1,279 @@
+<?php
+
+
+namespace Core\RPC;
+
+
+use Core\Context\ApplicationContext;
+use Core\Error\FormattedError;
+use Core\Serializer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class JSONP implements Handler {
+
+    /**
+     * @var string
+     */
+    protected $service;
+
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @var string
+     */
+    protected $clientName;
+
+    /**
+     * @var string
+     */
+    protected $clientVersion;
+
+    /**
+     * @var string
+     */
+    protected $locale;
+
+    /**
+     * @var string
+     */
+    protected $ipAddress;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * @var string
+     */
+    protected $returnedRootEntity;
+
+    /**
+     * @var string[]
+     */
+    protected $returnedFields;
+
+    /**
+     * @var string|null
+     */
+    protected $id;
+
+    /**
+     * @var string|null
+     */
+    protected $callback;
+
+    /**
+     * @return string
+     */
+    public function getClientName () {
+
+        return $this->clientName;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientVersion () {
+
+        return $this->clientVersion;
+
+    }
+
+    /**
+     * @param FormattedError $error
+     *
+     * @return Response
+     */
+    public function getErrorResponse (FormattedError $error) {
+
+        return new Response($this->getCallback() . '(' . json_encode(['jsonrpc' => '2.0',
+                                                                      'error'   => $error->toArray(),
+                                                                      'id'      => $this->getId(),
+                                                                     ]) . ')',
+                            Response::HTTP_OK, [
+                'Content-type'                => 'application/json',
+                'Access-Control-Allow-Origin' => '*'
+            ]);
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress () {
+
+        return $this->ipAddress;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale () {
+
+        return $this->locale;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod () {
+
+        return $this->method;
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams () {
+
+        return $this->params;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath () {
+
+        return $this->path;
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getReturnedFields () {
+
+        return $this->returnedFields;
+
+    }
+
+    /**
+     * @param string[] $fields
+     *
+     * @throws \Core\Error\FormattedError
+     */
+    protected function setReturnedFields (array $fields = NULL) {
+
+        $fields = (array)$fields;
+
+        $this->returnedFields = $fields;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnedRootEntity () {
+
+        return $this->returnedRootEntity;
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getService () {
+
+        return $this->service;
+
+    }
+
+    /**
+     * @param Serializer $serializer
+     * @param mixed      $data
+     *
+     * @return Response
+     */
+    public function getSuccessResponse (Serializer $serializer, $data) {
+
+        return new Response($this->getCallback() . '(' . json_encode(['jsonrpc' => '2.0',
+                                                                      'result'  => $serializer->serialize($data)->get(),
+                                                                      'id'      => $this->getId(),
+                                                                     ]) . ')',
+                            Response::HTTP_OK, [
+                'Content-type'                => 'application/json',
+                'Access-Control-Allow-Origin' => '*'
+            ]);
+
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @throws \Core\Error\FormattedError
+     */
+    public function parse (Request $request) {
+
+        $explodedPathInfo = explode('/', trim($request->getPathInfo(), '/'));
+
+        if (!isset($explodedPathInfo[1])) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_CLIENT_IS_INVALID);
+        }
+        $explodedClient = explode('+', $explodedPathInfo[1]);
+        if (count($explodedClient) != 3) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_CLIENT_IS_INVALID);
+        }
+        list($this->clientName, $this->clientVersion, $this->locale) = $explodedClient;
+        if ($this->locale != 'fr') {
+            $this->locale = 'en';
+        }
+
+        if (!isset($explodedPathInfo[2])) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_SERVICE_NOT_FOUND);
+        }
+        $this->service = $explodedPathInfo[2];
+
+        $this->method = $request->query->get('method');
+        if (!isset($this->method) || !is_string($this->method)) {
+            throw ApplicationContext::getInstance()->getErrorManager()->getFormattedError(ERR_METHOD_NOT_FOUND);
+        }
+
+        $this->callback = $request->query->get('callback');
+
+        $this->path = '/' . $this->service . '/' . $this->method;
+
+        $this->params = $request->query->get('params') ?: [];
+
+        $this->id = $request->query->get('id');
+
+        $this->ipAddress = $request->getClientIp();
+
+        $this->returnedRootEntity = $request->query->get('entity');
+        $this->setReturnedFields($request->query->get('fields'));
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getCallback () {
+
+        return $this->callback ?: 'callback';
+
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getId () {
+
+        return $this->id;
+
+    }
+
+}
---
+++ test/Core/RPC/JSONPTest.php
@@ -0,0 +1,125 @@
+<?php
+
+
+namespace Core\RPC;
+
+
+use Core\Context\RequestContext;
+use Core\Serializer;
+use Core\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class JSONPTest extends TestCase {
+
+    public static function setUpBeforeClass () {
+
+        self::getApplicationContext();
+
+    }
+
+    public function testParse () {
+
+        /**
+         * @var Request $req
+         */
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo','getClientIp']);
+        $req->method('getPathInfo')->willReturn('/protocol/client+version+locale/service/');
+        $req->method('getClientIp')->willReturn('10.0.1.104');
+        $params = ['param1' => 'value1', 'param2'];
+        $req->query->add(['method' => 'method', 'params' => $params]);
+        $JSONP = new JSONP();
+        $JSONP->parse($req);
+
+        $this->assertSame('/service/method', $JSONP->getPath());
+        $this->assertSame($params, $JSONP->getParams());
+        $this->assertSame('client', $JSONP->getClientName());
+        $this->assertSame('version', $JSONP->getClientVersion());
+        $this->assertSame('en', $JSONP->getLocale());
+        $this->assertNull($JSONP->getReturnedRootEntity());
+        $this->assertSame([], $JSONP->getReturnedFields());
+        $this->assertSame('service', $JSONP->getService());
+        $this->assertSame('method', $JSONP->getMethod());
+        $this->assertSame('10.0.1.104', $JSONP->getIpAddress());
+
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo']);
+        $req->method('getPathInfo')->willReturn('/protocol/client+version+fr/service/');
+        $params = [];
+        $req->query->add(['method' => 'method',
+                          'entity' => ($entity = 'entity'),
+                          'fields' => ($fields = ['field1', 'field2.subField1'])
+                         ]);
+        $JSONP = new JSONP();
+        $JSONP->parse($req);
+
+        $this->assertSame('/service/method', $JSONP->getPath());
+        $this->assertSame($params, $JSONP->getParams());
+        $this->assertSame('client', $JSONP->getClientName());
+        $this->assertSame('version', $JSONP->getClientVersion());
+        $this->assertSame('fr', $JSONP->getLocale());
+        $this->assertSame($entity, $JSONP->getReturnedRootEntity());
+        $this->assertSame($fields, $JSONP->getReturnedFields());
+        $this->assertSame('service', $JSONP->getService());
+        $this->assertSame('method', $JSONP->getMethod());
+
+        $response = $JSONP->getSuccessResponse(new Serializer(new RequestContext()), 'qwe');
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $response);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testClientNotFound () {
+
+        /**
+         * @var Request $req
+         */
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo']);
+        $req->method('getPathInfo')->willReturn('/protocol/');
+        (new JSONP())->parse($req);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testInvalidClient () {
+
+        /**
+         * @var Request $req
+         */
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo']);
+        $req->method('getPathInfo')->willReturn('/protocol/clientversion+locale/');
+        (new JSONP())->parse($req);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testServiceNotFound () {
+
+        /**
+         * @var Request $req
+         */
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo']);
+        $req->method('getPathInfo')->willReturn('/protocol/client+version+locale/');
+        (new JSONP())->parse($req);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testMethodNotFound () {
+
+        /**
+         * @var Request $req
+         */
+        $req = $this->getMock('\Symfony\Component\HttpFoundation\Request', ['getPathInfo']);
+        $req->method('getPathInfo')->willReturn('/protocol/client+version+locale/service');
+        (new JSONP())->parse($req);
+
+    }
+
+} 
