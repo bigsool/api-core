82bc86e74442df9a4041d7e3ca5de9c00ab60bbc
Julien <julien@bigsool.com>
2015-03-16 17:20:06 +0100

    fix Email and NotBlank Validation Parameter

--- src/Core/Validation/Parameter/Email.php
+++ src/Core/Validation/Parameter/Email.php
@@ -10,7 +10,7 @@ class Email extends Constraint {
     public function __construct () {
 
         $this->setConstraint(new Constraints\Email());
-        $this->setErrorCode(ERROR_INVALID_PARAM_STRING);
+        $this->setErrorCode(ERROR_INVALID_PARAM_EMAIL);
 
     }
 
