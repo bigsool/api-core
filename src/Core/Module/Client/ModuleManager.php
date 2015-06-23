f1e43ad534a778d09f9db116410986fd78490ab5
Julien <julien@bigsool.com>
2015-06-23 10:32:52 +0200

    add Actions in Client Module

--- src/Core/Module/Client/ModuleManager.php
+++ src/Core/Module/Client/ModuleManager.php
@@ -3,6 +3,9 @@
 
 namespace Core\Module\Client;
 
+use Core\Action\Action;
+use Core\Action\GenericAction;
+use Core\Context\ActionContext;
 use Core\Context\ApplicationContext;
 use Core\Module\ModuleEntityDefinition;
 use Core\Module\ModuleManager as AbstractModuleManager;
@@ -12,12 +15,44 @@ class ModuleManager extends AbstractModuleManager {
     /**
      * @param ApplicationContext $context
      *
+     * @return Action[]
+     */
+    public function createActions (ApplicationContext &$context) {
+
+        return [
+
+            new GenericAction('Core\Client','create',[],[],function(ActionContext $context){
+
+                $client = $this->getModuleEntity('Client')->create($context->getParams(), $context);
+                $this->getModuleEntity('Client')->save($client);
+
+                return $client;
+
+            }),
+
+            new GenericAction('Core\Client','createDevice',[],[],function(ActionContext $context){
+
+                $device = $this->getModuleEntity('Device')->create($context->getParams(), $context);
+                $this->getModuleEntity('Device')->save($device);
+
+                return $device;
+
+            }),
+
+        ];
+
+    }
+
+    /**
+     * @param ApplicationContext $context
+     *
      * @return ModuleEntityDefinition[]
      */
     public function getModuleEntitiesName (ApplicationContext &$context) {
 
         return [
-            'Client', 'Device'
+            'Client',
+            'Device',
         ];
 
     }
