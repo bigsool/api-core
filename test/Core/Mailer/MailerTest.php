8589adfc1264108e82a605264e753738d2864cb1
Julien <julien@bigsool.com>
2015-09-10 16:40:33 +0200

    fix MailerTest

--- test/Core/Mailer/MailerTest.php
+++ test/Core/Mailer/MailerTest.php
@@ -23,10 +23,15 @@ class MailerTest extends TestCase {
             (new Mailer($appCtx))->send('thierry@bigsool.com', 'resetYourPassword',
                                         'you can reset your password by clicking on this link');
         $this->assertInternalType('array', $result);
-        $this->assertTrue(count($result) == 1);
+        $this->assertCount(1, $result);
+        $this->assertArrayHasKey(0,$result);
+        $this->assertInternalType('array', $result[0]);
+        $this->assertArrayHasKey('email', $result[0]);
         $this->assertSame('thierry@bigsool.com', $result[0]['email']);
+        $this->assertArrayHasKey('status', $result[0]);
         $this->assertThat($result[0]['status'], new InArray(['sent', 'queued']));
-        $this->assertSame(NULL, $result[0]['reject_reason']);
+        $this->assertArrayHasKey('reject_reason', $result[0]);
+        $this->assertNull($result[0]['reject_reason']);
 
     }
 
@@ -37,10 +42,15 @@ class MailerTest extends TestCase {
             (new \Core\Mailer\Mailer($appCtx))->sendFromTemplate('test', 'thierry@bigsool.com',
                                                                  ['Name' => 'thierry'], 'resetYourPassword');
         $this->assertInternalType('array', $result);
-        $this->assertTrue(count($result) == 1);
+        $this->assertCount(1, $result);
+        $this->assertArrayHasKey(0,$result);
+        $this->assertInternalType('array', $result[0]);
+        $this->assertArrayHasKey('email', $result[0]);
         $this->assertSame('thierry@bigsool.com', $result[0]['email']);
+        $this->assertArrayHasKey('status', $result[0]);
         $this->assertThat($result[0]['status'], new InArray(['sent', 'queued']));
-        $this->assertSame(NULL, $result[0]['reject_reason']);
+        $this->assertArrayHasKey('reject_reason', $result[0]);
+        $this->assertNull($result[0]['reject_reason']);
 
     }
 
