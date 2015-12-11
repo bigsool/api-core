e0e1782528bf1e56491d6611b9c5bf6e02a22fe0
Julien <julien@bigsool.com>
2015-12-11 14:31:42 +0100

    add ArrayExtra::array_diff_assoc_recursive method

--- src/Core/Util/ArrayExtra.php
+++ src/Core/Util/ArrayExtra.php
@@ -181,4 +181,35 @@ class ArrayExtra {
 
     }
 
+    /**
+     * @see http://php.net/manual/fr/function.array-diff-assoc.php#111675
+     * @param array $array1
+     * @param array $array2
+     *
+     * @return array
+     */
+    public static function array_diff_assoc_recursive ($array1, $array2) {
+        $difference = array();
+        foreach ($array1 as $key => $value) {
+            if (is_array($value)) {
+                if (!isset($array2[$key]) || !is_array($array2[$key])) {
+                    $difference[$key] = $value;
+                }
+                else {
+                    $new_diff = static::array_diff_assoc_recursive($value, $array2[$key]);
+                    if (!empty($new_diff)) {
+                        $difference[$key] = $new_diff;
+                    }
+                }
+            }
+            else {
+                if (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
+                    $difference[$key] = $value;
+                }
+            }
+        }
+
+        return $difference;
+    }
+
 }
