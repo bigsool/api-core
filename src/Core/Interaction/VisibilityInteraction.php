85d7d7baa9ea68ccf8fd62f098360b6149e9808d
Léonard <leonard@bigsool.com>
2017-06-15 15:28:48 +0200

    VisibilityInteraction finish?

--- src/Core/Interaction/VisibilityInteraction.php
+++ src/Core/Interaction/VisibilityInteraction.php
@@ -6,4 +6,13 @@ class VisibilityInteraction extends AlertInteraction {
 
     const TYPE = "visibility";
 
+    public function toArray () {
+        return [
+             'type' => $this->getType(),
+             'message' => $this->getMessage(),
+             'topic' => $this->getTopic(),
+             'value' => true
+            ];
+    }
+
 }
