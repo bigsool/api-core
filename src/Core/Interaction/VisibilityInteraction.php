ece7db9263bd2a019d2de004c0909354c248b8d3
Léonard <leonard@bigsool.com>
2017-06-15 15:32:30 +0200

    refactor visibilityInteraction

--- src/Core/Interaction/VisibilityInteraction.php
+++ src/Core/Interaction/VisibilityInteraction.php
@@ -6,12 +6,22 @@ class VisibilityInteraction extends AlertInteraction {
 
     const TYPE = "visibility";
 
+    /**
+     * @var isDisplayed
+     */
+    protected $isDisplayed;
+
+    public function __construct ($isDisplayed) {
+        parent::__construct(null,null);
+        $this->isDisplayed = $isDisplayed;
+    }
+
     public function toArray () {
         return [
              'type' => $this->getType(),
              'message' => $this->getMessage(),
              'topic' => $this->getTopic(),
-             'value' => true
+             'value' => $this->isDisplayed
             ];
     }
 
