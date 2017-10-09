4208d9e5cb6e4e68c654d59a88804d2f6abe9d65
Ivan Delsinne <ivan@bigsool.com>
2017-10-09 14:37:03 +0200

    Changed PositiveInteger to Positive (broader check)

--- src/Core/Validation/Parameter/Positive.php
+++ src/Core/Validation/Parameter/Positive.php
@@ -9,7 +9,6 @@ class PositiveInteger extends Constraint {
 
     public function __construct () {
 
-        $this->setConstraint(new Constraints\Type(['type' => 'int']));
         $this->setConstraint(new Constraints\GreaterThanOrEqual(0));
         $this->setErrorCode(ERROR_INVALID_PARAM_INT);
 
