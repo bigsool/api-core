949648e053686e6b96b0b79f2b310292877b5330
Ivan Delsinne <ivan@bigsool.com>
2017-10-10 09:41:41 +0200

    Added type hinting in limit/offset functions

--- src/Core/Context/FindQueryContext.php
+++ src/Core/Context/FindQueryContext.php
@@ -59,24 +59,24 @@ class FindQueryContext implements QueryContext {
     /**
      * @param $limit
      */
-    public function setLimit($limit) {
+    public function setLimit(int $limit) {
         $this->limit = $limit;
     }
     /**
      * @return int
      */
-    public function getLimit() { return $this->limit; }
+    public function getLimit(): int { return $this->limit; }
 
     /**
      * @param $offset
      */
-    public function setOffset($offset) {
+    public function setOffset(int $offset) {
         $this->offset = $offset;
     }
     /**
      * @return int
      */
-    public function getOffset() { return $this->offset; }
+    public function getOffset(): int { return $this->offset; }
 
     /**
      * @param string         $entity
